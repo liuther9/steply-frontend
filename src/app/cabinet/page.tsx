6c6a0b011ce7b0d3cbd35fe81d104af8fd9c4dbd
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2023-05-05 17:16:28 +0300

    add publication component

--- src/app/cabinet/page.tsx
+++ src/app/cabinet/page.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import { BiCoin } from 'react-icons/bi';
-// import { AiOutlineRight } from 'react-icons/ai';
+import { AiOutlineRight } from 'react-icons/ai';
+import { AiOutlineEye } from 'react-icons/ai';
 import { NextPage } from 'next';
 import s from './page.module.scss';
 import Card from '../cabinet/_components/index';
@@ -38,57 +40,25 @@ const Cabinet: NextPage = (p: any) => {
         </div>
         <button className={s.button}>Пройти анкету</button>
       </div>
-      <h2 className={s.hwTitle}>Домашние рыботы</h2>
+      <h2 className={s.hwTitle}>Домашние работы</h2>
       <div className={s.hwWrapper}>
         {data.map(i => (
           <Card cityName={i.cityName} key={i.id} />
         ))}
       </div>
-
-      {/* <div className={s.hwWrapper}>
-        <ul className={s.hwList}>
-          <li className={s.hwItem}>
-            <h3 className={s.cityName}>Чехия</h3>
-            <p className={s.phase}>Этап 1</p>
-            <span className={s.hwText}>
-              {' '}
-              Посмотрите все сайты заданных университетов и составьте список,
-              что вам подходит по специальности
-            </span>
-            <AiOutlineRight className={s.reactIcon} />
-          </li>
-
-          <li className={s.hwItem}>
-            <h3 className={s.cityName}>Профориентация</h3>
-            <p className={s.phase}>Этап 1</p>
-            <span className={s.hwText}>
-              Посмотрите все сайты заданных университетов и составьте список,
-              что вам подходит по специальности
-            </span>
-            <AiOutlineRight className={s.reactIcon} />
-          </li>
-
-          <li className={s.hwItem}>
-            <h3 className={s.cityName}>Германия</h3>
-            <p className={s.phase}>Этап 1</p>
-            <span className={s.hwText}>
-              Посмотрите все сайты заданных университетов и составьте список,
-              что вам подходит по специальности
-            </span>
-            <AiOutlineRight className={s.reactIcon} />
-          </li>
-
-          <li className={s.hwItem}>
-            <h3 className={s.cityName}>Турция</h3>
-            <p className={s.phase}>Этап 1</p>
-            <span className={s.hwText}>
-              Посмотрите все сайты заданных университетов и составьте список,
-              что вам подходит по специальности
-            </span>
-            <AiOutlineRight className={s.reactIcon} />
-          </li>
-        </ul>
-      </div> */}
+      <h3 className={s.hwTitle}>Мои публикации</h3>
+      <div className={s.publication}>
+        <p className={s.publicationTitle}>Как я тут оказался?</p>
+        <span className={s.publicationText}>
+          Мое путешествие началось во время пандемии...
+        </span>
+        <Link className={s.publicationLink} href="#china">
+          #china
+        </Link>
+        <AiOutlineRight className={s.reactIcon} />
+        <span className={s.publicationNum}>9780</span>
+        <AiOutlineEye className={s.eyeIcon} />
+      </div>
     </section>
   );
 };
