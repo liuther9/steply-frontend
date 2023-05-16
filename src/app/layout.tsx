d1dce8f3d04c805c8b21cb84f43d7bb26d823d64
liuther9 <zd@akhter.studio>
2023-05-16 19:12:30 +0600

    feat: metrics and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import YandexMetrics from '@/utils/YandexMetrics'
 import './globals.css'
 import SupabaseProvider from '@utils/supabase-provider'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { META_PIXEL_ID, YANDEX_METRICS_ID } from '@/constants'
+import { MetaPixelMetrics } from '@/utils/MetaPixelMetrics'
 
 const inter = Inter({
 	subsets: ['cyrillic', 'latin'],
@@ -15,7 +19,23 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html className={inter.className} lang='en'>
+			<Head>
+				<noscript>
+					{/* eslint-disable-next-line @next/next/no-img-element */}
+					<img alt='' src={`https://mc.yandex.ru/watch/${YANDEX_METRICS_ID}`} style={{ position: 'absolute', left: '-9999px' }} />
+					{/* eslint-disable-next-line @next/next/no-img-element */}
+					<img
+						alt=''
+						height='1'
+						src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+						style={{ display: 'none' }}
+						width='1'
+					/>
+				</noscript>
+			</Head>
 			<body>
+				<YandexMetrics ymId={YANDEX_METRICS_ID} />
+				<MetaPixelMetrics fbId={META_PIXEL_ID} />
 				<SupabaseProvider>
 					{children}
 					<div id='portal' />
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const YANDEX_METRICS_ID = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID || ''
+export const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || ''
